7cd67c9f5a127d9299af19633bbd9fddaeb14ec1
Victória César <victoriacesaras@gmail.com>
2022-03-24 08:54:08 -0300

    storage provider

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,4 +1,5 @@
 import "reflect-metadata";
+import "dotenv/config";
 import express, { NextFunction, Request, response, Response } from "express";
 import swaggerUi from "swagger-ui-express";
 import "express-async-errors";
